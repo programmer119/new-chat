cffd59b956aa12e7e02253c824e74c781427cacc
programmer119 <53187583+programmer119@users.noreply.github.com>
2026-05-11 06:10:04 +0900

    Bust v1 web asset cache

--- bullet-noir-v1/ringedfs.js
+++ bullet-noir-v1/ringedfs.js
@@ -1,4 +1,4 @@
 if (!Module.preRun) Module.preRun = [];
 Module.preRun.push(function() {
-  FS.createPreloadedFile("/", "data.pck", "data.pck", true, true);
+  FS.createPreloadedFile("/", "data.pck", "data.pck?v=v1-godot-startup-2", true, true);
 });
